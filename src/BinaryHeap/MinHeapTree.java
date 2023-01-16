90f7c81566b0fc00eb57db4306cce0821f4b1ae3
anuuet <anuuet@ttu.ee>
2023-01-16 17:53:39 +0200

    Kahendkuhi puu kujul

---
+++ src/BinaryHeap/MinHeapTree.java
@@ -0,0 +1,86 @@
+package BinaryHeap;
+
+public class MinHeapTree {
+	private static boolean isHeap(Node root) {
+		if (root.getLeft() == null && root.getRight() == null) {
+			return true;
+		}
+		if (root.getLeft() == null) {
+			return root.getValue() <= root.getRight().getValue();
+		}
+		if (root.getRight() == null) {
+			return root.getValue() <= root.getLeft().getValue();
+		}
+
+		return root.getValue() <= root.getLeft().getValue()
+				&& root.getValue() <= root.getRight().getValue()
+				&& isHeap(root.getLeft())
+				&& isHeap(root.getRight());
+	}
+
+	private static Node insert(Node root, int value) {
+		Node newNode = new Node(value);
+		if (root.getValue() == null) {
+			return newNode;
+		}
+		insertNode(root, newNode);
+		return heapify(root);
+	}
+
+	private static Node insertNode(Node root, Node newNode) {
+		if (root.getLeft() == null) {
+			root.setLeft(newNode);
+			return heapify(root);
+		}
+		if (root.getRight() == null) {
+			root.setRight(newNode);
+			return heapify(root);
+		}
+		if (root.getBalanceFactor() == -1) {
+			if (root.getLeft().getBalanceFactor() == -1) {
+				root.setLeft(insertNode(root.getLeft(), newNode));
+			} else {
+				root.setRight(insertNode(root.getRight(), newNode));
+			}
+		} else {
+			if (root.getRight().getBalanceFactor() == -1) {
+				root.setRight(insertNode(root.getRight(), newNode));
+			} else {
+				root.setLeft(insertNode(root.getLeft(), newNode));
+			}
+		}
+		return heapify(root);
+	}
+
+	private static Node heapify(Node root) {
+		if (root.getLeft() != null) {
+			if (root.getLeft().getValue() < root.getValue()) {
+				root.getLeft().setValue(root.getLeft().getValue() + root.getValue());
+				root.setValue(root.getLeft().getValue() - root.getValue());
+				root.getLeft().setValue(root.getLeft().getValue() - root.getValue());
+			}
+		}
+		if (root.getRight() != null) {
+			if (root.getRight().getValue() < root.getValue()) {
+				root.getRight().setValue(root.getRight().getValue() + root.getValue());
+				root.setValue(root.getRight().getValue() - root.getValue());
+				root.getRight().setValue(root.getRight().getValue() - root.getValue());
+			}
+		}
+		return root;
+	}
+
+	public static void main(String[] args) {
+		Node root = new Node();
+		int[] values = new int[] {28, 90, 88, 3, 65, 97, 17, 31, 99, 98};
+		for (int value : values) {
+			root = insert(root, value);
+			System.out.println(root.pseudoXMLRepresentation());
+		}
+		if (isHeap(root)) {
+			System.out.println("Yes");
+		} else {
+			System.out.println("No");
+		}
+	}
+}
---
+++ src/BinaryHeap/Node.java
@@ -0,0 +1,121 @@
+package BinaryHeap;
+
+class Node {
+
+	private Integer value;
+	private Node left;
+	private Node right;
+
+	Node() {
+		this.value = null;
+	}
+
+	Node(int value) {
+		this.value = value;
+	}
+
+	public Integer getValue() {
+		return value;
+	}
+
+	public void setValue(int value) {
+		this.value = value;
+	}
+
+	public Node getLeft() {
+		return left;
+	}
+
+	public void setLeft(Node left) {
+		this.left = left;
+	}
+
+	public Node getRight() {
+		return right;
+	}
+
+	public void setRight(Node right) {
+		this.right = right;
+	}
+
+	int getMinHeight() {
+		return getMinHeightSubtree(1);
+	}
+
+	private int getMinHeightSubtree(int layer) {
+		int height = 0;
+		if (getLeft() == null && getRight() == null) {
+			height = layer;
+		}
+		if (getLeft() != null) {
+			int newHeight = getLeft().getMinHeightSubtree(layer + 1);
+			if (newHeight < height || height == 0) {
+				height = newHeight;
+			}
+		}
+		if (getRight() != null) {
+			int newHeight = getRight().getMinHeightSubtree(layer + 1);
+			if (newHeight < height || height == 0) {
+				height = newHeight;
+			}
+		}
+		return height;
+	}
+
+	int getBalanceFactor() {
+		int rightHeight = getRight() != null ? getRight().getMinHeight() : 0;
+		int leftHeight = getLeft() != null ? getLeft().getMinHeight() : 0;
+		return rightHeight - leftHeight;
+	}
+
+	public String pseudoXMLRepresentation() {
+		StringBuilder XML = new StringBuilder();
+		fillXML(XML, 1);
+		return XML.toString();
+	}
+
+	private void fillXML(StringBuilder XML, int layer) {
+		XML.append("\t".repeat(layer - 1));
+		XML.append("<L");
+		XML.append(layer);
+		XML.append("> ");
+		XML.append(getValue());
+		XML.append(' ');
+		if (getLeft() != null) {
+			XML.append("\n");
+			getLeft().fillXML(XML, ++layer);
+			layer--;
+			XML.append("\t".repeat(Math.max(0, layer - 1)));
+		} else {
+			XML.append("\n");
+			XML.append("\t".repeat(layer));
+			XML.append("<L");
+			XML.append(++layer);
+			XML.append("> ");
+			XML.append("</L");
+			XML.append(layer--);
+			XML.append(">");
+		}
+		if (getRight() != null) {
+			XML.append("\n");
+			getRight().fillXML(XML, ++layer);
+			layer--;
+			XML.append("\t".repeat(Math.max(0, layer - 3)));
+		} else {
+			XML.append("\n");
+			XML.append("\t".repeat(layer));
+			XML.append("<L");
+			XML.append(++layer);
+			XML.append("> ");
+			XML.append("</L");
+			XML.append(layer--);
+			XML.append(">\n");
+		}
+		XML.append("\t".repeat(layer - 1));
+		XML.append("</L");
+		XML.append(layer);
+		XML.append(">");
+		XML.append("\n");
+	}
+}
+
