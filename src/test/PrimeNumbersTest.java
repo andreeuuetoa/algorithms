06e8fc0cfd1e4c1f12db9d607d360170a196cb00
andreeuuetoa <andree.uuetoa@gmail.com>
2023-10-13 22:03:25 +0300

    PrimeNumbers tests

---
+++ src/test/PrimeNumbersTest.java
@@ -0,0 +1,31 @@
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class PrimeNumbersTest {
+    @Test
+    public void testPrimeNumbersUpTo100() {
+        List<Integer> primeNumbersTo100 = new PrimeNumberGenerator().generatePrimesTo(100);
+        assertEquals(primeNumbersTo100.size(), 25);
+    }
+
+    @Test
+    public void testPrimeNumbersUpTo1000() {
+        List<Integer> primeNumbersTo100 = new PrimeNumberGenerator().generatePrimesTo(1000);
+        assertEquals(primeNumbersTo100.size(), 168);
+    }
+
+    @Test
+    public void testPrimeNumbersUpTo10000() {
+        List<Integer> primeNumbersTo100 = new PrimeNumberGenerator().generatePrimesTo(10000);
+        assertEquals(primeNumbersTo100.size(), 1229);
+    }
+
+    @Test
+    public void testPrimeNumbersUpTo100000() {
+        List<Integer> primeNumbersTo100 = new PrimeNumberGenerator().generatePrimesTo(100000);
+        assertEquals(primeNumbersTo100.size(), 9592);
+    }
+}
