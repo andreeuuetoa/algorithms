90de464adbcd167a637257e82c09fd51fd7db220
andreeuuetoa <andree.uuetoa@gmail.com>
2023-10-15 00:46:04 +0300

    New package for prime number generator

--- src/main/java/mathematics/PrimeNumberGenerator.java
+++ src/main/java/mathematics/PrimeNumberGenerator.java
@@ -1,3 +1,5 @@
+package mathematics;
+
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
--- src/test/java/PrimeNumbersTest.java
+++ src/test/java/PrimeNumbersTest.java
@@ -1,3 +1,4 @@
+import mathematics.PrimeNumberGenerator;
 import org.junit.jupiter.api.Test;
 
 import java.util.List;
